1118d6d50ced298c3d7425dbc89a1bcbfcb5779c
Jean Domingues <jeandomingues752@gmail.com>
2020-08-06 15:29:45 -0300

    Estilizando a página de cadastro

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
+
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
 
 import './styles.css';
 
@@ -15,23 +19,26 @@ function TeacherForm() {
         <fieldset>
           <legend>Seus dados</legend>
 
-          <div className="input-block">
-            <label htmlFor="name">Nome completo</label>
-            <input type="text" id="name" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="avatar">Avatar</label>
-            <input type="text" id="avatar" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="whatsapp">Whatsapp</label>
-            <input type="text" id="whatsapp" />
-          </div>
-
-
+          <Input name="name" label="Nome completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="Whatsapp" />
         </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+          <Input name="subject" label="Matéria" />
+          <Input name="cost" label="Custo da hora/aula" />
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante" />
+            Importante! <br />
+            Preencha todos os dados
+          </p>
+
+          <button type="button">Salvar cadastro</button>
+        </footer>
       </main>
     </div>
   );
