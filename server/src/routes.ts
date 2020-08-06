1044d5d9c1779b8c933c70456294505f2026737e
Jean Domingues <jeandomingues752@gmail.com>
2020-08-05 22:49:10 -0300

    Rota de conexõers criada

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,60 +1,16 @@
 import express from 'express';
-import db from './database/connection';
-import convertHoursToMinute from './utils/convertHoursToMinutes';
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = express.Router();
+const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
 
-interface ScheduleItem {
-  week_day: number;
-  from: string;
-  to: string;
-}
 
-routes.post('/classes', async (req, res) => {
-  const { name, avatar, whatsapp, bio, subject, cost, schedule } = req.body;
+routes.get('/classes', classesController.index);
+routes.post('/classes', classesController.create);
 
-  const trx = await db.transaction();
-
-  try {
-    const insertUsersIds = await trx('users').insert({
-      name,
-      avatar,
-      whatsapp,
-      bio,
-    });
-
-    const user_id = insertUsersIds[0];
-
-    const insertedClassesIds = await trx('classes').insert({
-      subject,
-      cost,
-      user_id,
-    });
-
-    const class_id = insertedClassesIds[0];
-
-    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-      return {
-        class_id,
-        week_day: scheduleItem.week_day,
-        from: convertHoursToMinute(scheduleItem.from),
-        to: convertHoursToMinute(scheduleItem.to),
-      };
-    });
-
-    await trx('class_schedule').insert(classSchedule);
-
-    await trx.commit();
-
-    return res.status(201).json({ succes: true });
-
-  } catch (err) {
-    await trx.rollback();
-
-    return res.status(400).json({
-      error: 'Unexpected error while creating new class',
-    });
-  }
-});
+routes.post('/connections', connectionsController.create)
+routes.get('/connections', connectionsController.index)
 
 export default routes;
