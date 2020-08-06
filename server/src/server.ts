6b61d58694349dbd1751fca07e0aa9178bf387fa
Jean Domingues <jeandomingues752@gmail.com>
2020-08-05 21:31:37 -0300

    Criando a rota para cadastro de aulas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,9 @@
-import express, { json } from 'express';
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.post('/', (req, res) => {
-  return res.json({ message: 'tudo certo'});
-});
+app.use(routes);
 
 app.listen(3333);
