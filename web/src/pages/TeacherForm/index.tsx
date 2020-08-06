73a72f29b92f51dd5e0d556f3ac6abb7ce1db0c3
Jean Domingues <jeandomingues752@gmail.com>
2020-08-06 16:42:21 -0300

    Separando o formulário em componentes

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -4,6 +4,8 @@ import warningIcon from '../../assets/images/icons/warning.svg';
 
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
+import TextArea from '../../components/Textarea';
+import Select from '../../components/Select';
 
 import './styles.css';
 
@@ -22,11 +24,29 @@ function TeacherForm() {
           <Input name="name" label="Nome completo" />
           <Input name="avatar" label="Avatar" />
           <Input name="whatsapp" label="Whatsapp" />
+          <TextArea name="bio" label="Biografia" />
         </fieldset>
 
         <fieldset>
           <legend>Sobre a aula</legend>
-          <Input name="subject" label="Matéria" />
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Ciências', label: 'Ciências' },
+              { value: 'Educação Física', label: 'Educação Física' },
+              { value: 'Matemática', label: 'Matemática' },
+              { value: 'Português', label: 'Português' },
+              { value: 'Inglês', label: 'Inglês' },
+              { value: 'Química', label: 'Química' },
+              { value: 'Sociologia', label: 'Sociologia' },
+              { value: 'História', label: 'História' },
+              { value: 'Geografia', label: 'Geografia' },
+              { value: 'Física', label: 'Física' },
+            ]}
+          />
           <Input name="cost" label="Custo da hora/aula" />
         </fieldset>
 
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 import './styles.css';
 
@@ -11,8 +12,38 @@ function TeacherList() {
     <div id="page-teacher-list" className="container">
       <PageHeader title="Este são os professores disponíveis.">
         <form id="search-teachers">
-          <Input name="subject" label="Matéria" type="text" />
-          <Input name="week_day" label="Dia da semana" type="text" />
+        <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Ciências', label: 'Ciências' },
+              { value: 'Educação Física', label: 'Educação Física' },
+              { value: 'Matemática', label: 'Matemática' },
+              { value: 'Português', label: 'Português' },
+              { value: 'Inglês', label: 'Inglês' },
+              { value: 'Química', label: 'Química' },
+              { value: 'Sociologia', label: 'Sociologia' },
+              { value: 'História', label: 'História' },
+              { value: 'Geografia', label: 'Geografia' },
+              { value: 'Física', label: 'Física' },
+            ]}
+          />
+
+        <Select
+            name="week_day"
+            label="Dia da semana"
+            options={[
+              { value: '0', label: 'Domingo' },
+              { value: '1', label: 'Segunda-feira' },
+              { value: '2', label: 'Terça-feira' },
+              { value: '3', label: 'Quarta-feira' },
+              { value: '4', label: 'Quinta-feira' },
+              { value: '5', label: 'Sexta-feira' },
+              { value: '6', label: 'Sábado' },
+            ]}
+          />
           <Input name="time" label="Hora" type="text" /> 
         </form>
       </PageHeader>
